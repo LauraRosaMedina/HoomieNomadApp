c312c1d1d5568a3bd52f566620e4740d76a805be
LauraRosaMedina <157789658+LauraRosaMedina@users.noreply.github.com>
2024-04-10 10:56:39 +0200

    Update Prueba1ejercicio.java

--- src/Prueba2/Prueba1ejercicio.java
+++ src/Prueba2/Prueba1ejercicio.java
@@ -5,6 +5,7 @@ public class Prueba1ejercicio {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola David, te amo");
+		System.out.println("Hola Mar, te amo");
 	}
 
 }
